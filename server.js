a78c15aec19887f840ebab2c41b6d41f78047757
Adeyeye <x638132@am.abg.com>
2019-11-05 06:42:01 -0600

    new commit

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+life is good
+
 
 const PORT = process.env.PORT || 5000;
 
